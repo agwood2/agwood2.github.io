ad5003151863093b5c6f1427765de8a5ac05f91d
internet-class.org Student <agwood2@buffalo.edu>
2016-12-14 02:31:30 -0500

    new commit

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+$(document).ready(function()){
+  var nav = $('nav');
+  var li = $('nav li');
+
 $('nav').on('click',function()){
   var elementTo = $(this).attr('data-sectionid');
   $('html, body').animate({scrollTop: $('#'+elementTo).offset().top-80}, 2000);
 });
+}
 }
