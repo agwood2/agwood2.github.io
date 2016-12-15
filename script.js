c507c92e060b98322f5d1293b03347764c212ff7
agwood2 <agwood12@gmail.com>
2016-12-14 23:15:43 -0500

    new commit

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
-$(document).ready(function()){
-  var nav = $('nav');
-  var li = $('nav li');
+// $(document).ready(function()){
+//   var nav = $('nav');
+//   var li = $('nav li');
+//
+// $('nav a').on('click',function()){
+//   var elementTo = $(this).attr('data-sectionid');
+//   $('html, body').animate({scrollTop: $('#'+elementTo).offset().top-80}, 2000);
+// });
+// }
+// }
 
-$('nav a').on('click',function()){
-  var elementTo = $(this).attr('data-sectionid');
-  $('html, body').animate({scrollTop: $('#'+elementTo).offset().top-80}, 2000);
+<script type="text/javascript">
+$(document).ready(function(){
+  $('#navigation a, #fixedbar a').on('click', function(e) {
+    e.preventDefault();
+  });
+
+  $(window).on('scroll',function() {
+    var scrolltop = $(this).scrollTop();
+
+    if(scrolltop >= 215) {
+      $('#fixedbar').fadeIn(250);
+    }
+
+    else if(scrolltop <= 210) {
+      $('#fixedbar').fadeOut(250);
+    }
+  });
 });
-}
-}
+</script>
